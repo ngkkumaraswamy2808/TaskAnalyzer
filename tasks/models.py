47fbe23182b26b02ceed88efce2f9719b72147bc
N .G.K.Kumara Swamy <swamychinnu464@gmail.com>
2025-11-30 14:22:02 +0530

    feat: add Task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    due_date = models.DateField(null=True, blank=True)
+    importance = models.IntegerField(default=5)  # 1-10
+    estimated_hours = models.IntegerField(default=1)
+    # list of task IDs that this task depends on
+    dependencies = models.JSONField(default=list, blank=True)
+
+    def _str_(self):
+        return self.title
